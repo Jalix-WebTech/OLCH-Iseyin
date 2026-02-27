ebc3ef109ad90fcacad6c7f2ae7282b8fab10f72
Jalix-WebTech <jalixwebtech@gmail.com>
2026-02-27 13:59:56 +0100

    Updating auto animate service's section

--- JS/home-page-auto-scroll.js
+++ JS/home-page-auto-scroll.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const slides = document.getElementById("slides");
+  const wrapper = document.querySelector(".slides-wrapper");
   const dotsContainer = document.getElementById("dots");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
   const cards = document.querySelectorAll(".story-card");
   const slider = document.querySelector(".slider");
 
   let index = 0;
+  let cardWidth = 0;
   let visibleCards = 1;
   let totalSteps = 1;
-  let cardWidth = 0;
   let interval;
 
   function calculate() {
     cardWidth = cards[0].offsetWidth + 16;
-    visibleCards = Math.floor(slider.offsetWidth / cardWidth);
+    visibleCards = Math.floor(wrapper.offsetWidth / cardWidth);
     totalSteps = cards.length - visibleCards + 1;
 
     if (totalSteps < 1) totalSteps = 1;
@@ -49,21 +52,35 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function autoSlide() {
-    index++;
-    if (index >= totalSteps) {
-      index = 0;
+    if (window.innerWidth >= 769) {
+      index++;
+      if (index >= totalSteps) index = 0;
+      updateSlider();
     }
-    updateSlider();
   }
 
   function startAuto() {
-    interval = setInterval(autoSlide, 2500);
+    interval = setInterval(autoSlide, 3000);
   }
 
   function restartAuto() {
     clearInterval(interval);
     startAuto();
   }
+
+  prevBtn.addEventListener("click", function () {
+    if (index > 0) index--;
+    else index = totalSteps - 1;
+    updateSlider();
+    restartAuto();
+  });
+
+  nextBtn.addEventListener("click", function () {
+    index++;
+    if (index >= totalSteps) index = 0;
+    updateSlider();
+    restartAuto();
+  });
 
   calculate();
   window.addEventListener("resize", calculate);
