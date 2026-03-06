8446af3448616fff60fc21ccf77a04585040ea07
Jalix-WebTech <jalixwebtech@gmail.com>
2026-03-06 03:00:51 +0100

    updates

--- JS/service.js
+++ JS/service.js
@@ -2,35 +2,49 @@
 ================================
 SERVICE FILTER FUNCTION
 ================================
-This function shows services
-based on selected category
+Filters services and updates
+active button with fade effect
 */
 
-function filterServices(category){
+function filterServices(category, button){
 
 let cards = document.querySelectorAll(".service-card");
+let buttons = document.querySelectorAll(".filter-btn");
+
+
+/* remove active state from all buttons */
+
+buttons.forEach(function(btn){
+
+btn.classList.remove("active");
+
+});
+
+
+/* activate clicked button */
+
+button.classList.add("active");
+
+
+/* filter cards */
 
 cards.forEach(function(card){
 
 if(category === "all"){
 
-card.style.display = "block";
+card.style.display="block";
+
+}
+
+else if(card.classList.contains(category)){
+
+card.style.display="block";
 
 }
 
 else{
 
-if(card.classList.contains(category)){
-
-card.style.display = "block";
-
-}
-
-else{
-
-card.style.display = "none";
-
-}
+card.style.display="none";
 
 }
 
@@ -42,28 +56,35 @@ card.style.display = "none";
 
 /*
 ================================
-READ MORE / READ LESS FUNCTION
+READ MORE FUNCTION
 ================================
-Expands hidden service text
+Expands card text and adjusts
+image to show full content
 */
 
 function toggleText(button){
 
-let moreText = button.parentElement.querySelector(".more-text");
+let card = button.closest(".service-card");
+let moreText = card.querySelector(".more-text");
 
-if(moreText.style.display === "none" || moreText.style.display === ""){
 
-moreText.style.display = "block";
+if(moreText.style.display === "block"){
 
-button.innerText = "Read Less";
+moreText.style.display="none";
+button.innerText="Read All";
+
+/* return image to cover */
+card.classList.remove("expanded");
 
 }
 
 else{
 
-moreText.style.display = "none";
+moreText.style.display="block";
+button.innerText="Read Less";
 
-button.innerText = "Read All";
+/* show full image */
+card.classList.add("expanded");
 
 }
 
